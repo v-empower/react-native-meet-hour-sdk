82f2b790ab193d26c28e0f0ae06c224897c3bb25
Mohammed Abdul Majeed <moham.majeed@v-empower.com>
2021-06-28 08:55:12 -0400

    updated methods for android/ios in RNMeetHourModule.java & RNMeetHourViewManager.m

--- android/src/main/java/go/meethour/io/react/sdk/RNMeetHourModule.java
+++ android/src/main/java/go/meethour/io/react/sdk/RNMeetHourModule.java
@@ -37,7 +37,7 @@ public class RNMeetHourModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void call(ReadableMap userInfo) {
+    public void join_meethour(ReadableMap userInfo) {
         RNMeetHourUserInfo _userInfo = new RNMeetHourUserInfo();
         UiThreadUtil.runOnUiThread(() -> {
 
@@ -216,6 +216,72 @@ public class RNMeetHourModule extends ReactContextBaseJavaModule {
                     .setVideoMuted(videoMuted)
                     .build();
             mMeetHourViewReference.getMeetHourView().join(meetHourOptions);
+        });
+    }
+
+    @ReactMethod
+    public void call(String url, ReadableMap userInfo) {
+        UiThreadUtil.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (mMeetHourViewReference.getMeetHourView() != null) {
+                    RNMeetHourUserInfo _userInfo = new RNMeetHourUserInfo();
+                    if (userInfo != null) {
+                        if (userInfo.hasKey("displayName")) {
+                            _userInfo.setDisplayName(userInfo.getString("displayName"));
+                          }
+                          if (userInfo.hasKey("email")) {
+                            _userInfo.setEmail(userInfo.getString("email"));
+                          }
+                          if (userInfo.hasKey("avatar")) {
+                            String avatarURL = userInfo.getString("avatar");
+                            try {
+                                _userInfo.setAvatar(new URL(avatarURL));
+                            } catch (MalformedURLException e) {
+                            }
+                          }
+                    }
+                    RNMeetHourConferenceOptions options = new RNMeetHourConferenceOptions.Builder()
+                            .setRoom(url)
+                            .setAudioOnly(false)
+                            .setUserInfo(_userInfo)
+                            .build();
+                    mMeetHourViewReference.getMeetHourView().join(options);
+                }
+            }
+        });
+    }
+
+    @ReactMethod
+    public void audioCall(String url, ReadableMap userInfo) {
+        UiThreadUtil.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (mMeetHourViewReference.getMeetHourView() != null) {
+                    RNMeetHourUserInfo _userInfo = new RNMeetHourUserInfo();
+                    if (userInfo != null) {
+                        if (userInfo.hasKey("displayName")) {
+                            _userInfo.setDisplayName(userInfo.getString("displayName"));
+                          }
+                          if (userInfo.hasKey("email")) {
+                            _userInfo.setEmail(userInfo.getString("email"));
+                          }
+                          if (userInfo.hasKey("avatar")) {
+                            String avatarURL = userInfo.getString("avatar");
+                            try {
+                                _userInfo.setAvatar(new URL(avatarURL));
+                            } catch (MalformedURLException e) {
+                            }
+                          }
+                    }
+                    RNMeetHourConferenceOptions options = new RNMeetHourConferenceOptions.Builder()
+                            .setRoom(url)
+                            .setAudioOnly(true)
+                            .setUserInfo(_userInfo)
+                            .build();
+                    mMeetHourViewReference.getMeetHourView().join(options);
+                }
+            }
         });
     }
 
