f87381e14f2302f9e76488fb0fe4a4773459a063
fareed069417 <shoeb.ahmad@v-empower.com>
2023-02-21 17:10:18 +0530

    API Calls Implementations - api.meethour.io

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
 export const GRANT_TYPE = 'password';
 export const GRANT_REFRESH_TYPE = 'refresh_token';
 export const API_BASE_URL = 'api.meethour.io';
-export const API_VERSION = 'v1.2';
+export const API_VERSION = 'v1.1';
